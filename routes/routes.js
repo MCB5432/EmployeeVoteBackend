e902cbc0c85fd183a3bd27f2f07d621fd9ed7e19
atilayeseN <atilayesen@hotmail.com>
2022-07-01 22:56:11 +0300

    image upload function

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const employeeModel = require("../models/employee");
 const router = express.Router();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 router.post("/create", async (req, res) => {
   const employee = new employeeModel({
@@ -14,11 +16,20 @@ router.post("/create", async (req, res) => {
     phone: req.body.phone,
   });
   try {
+    
     const savedUser = await employee.save();
     res.send(savedUser);
   } catch (error) {
     res.status(400).send(error);
   }
 });
+
+router.post("/image-upload", upload.array("picture"),uploadFiles);
+
+function uploadFiles(req, res) {
+  console.log(req.body);
+  console.log(req.files);
+  res.json({ message: "Successfully uploaded files" });
+}
 
 module.exports = router;
