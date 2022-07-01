496c0d30fa825fac745c29fe31eb60b1f40c4246
mert <mertcanbenli99@outlook.com>
2022-07-01 17:14:16 +0300

    Add employee

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require('./routes/routes');
+const routes = require("./routes/routes");
+require("dotenv").config();
 
-const url = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.0"
+const url = process.env.DBSTRING;
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 const database = mongoose.connection;
 
-database.on('error', (error) => {
-    console.log(error);
+database.on("error", (error) => {
+  console.log(error);
 });
-database.once('connected', () => {
-    console.log('Database Conneced');
+database.once("connected", () => {
+  console.log("Database Conneced");
 });
-
-
 
 const app = express();
 app.use(express.json());
-app.use('/api', routes)
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", routes);
+app.get('/',(req,res) => {
+  res.send('Hello world')
+})
 
 const port = process.env.PORT || 8000;
 
-
-
 app.listen(port, () => {
-    console.log(`Server listening on ${port}`)
-})
-
-
+  console.log(`Server listening on ${port}`);
+});
--- routes/routes.js
+++ routes/routes.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const employeeModel = require('../models/employee');
-const router = express.Router()
+const express = require("express");
+const employeeModel = require("../models/employee");
+const router = express.Router();
 
-router.post('/create', (req,res) => {
-    const employee = new employeeModel({
-        name: req.body.name,
-        surname: req.body.surname,
-        position: req.body.position,
-        gender: req.body.gender,
-        picture: req.body.picture,
-        point: req.body.point,
-        email: req.body.email,
-        phone: req.body.phone,
-    });
-    try {
-        employee.save();
-        res.status(200).json(employee);
-    } catch(error) {
-        res.status(400).json({message: error.message});
-    }
+router.post("/create", async (req, res) => {
+  const employee = new employeeModel({
+    name: req.body.name,
+    surname: req.body.surname,
+    position: req.body.position,
+    gender: req.body.gender,
+    picture: req.body.picture,
+    point: req.body.point,
+    email: req.body.email,
+    phone: req.body.phone,
+  });
+  try {
+    const savedUser = await employee.save();
+    res.send(savedUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 module.exports = router;
