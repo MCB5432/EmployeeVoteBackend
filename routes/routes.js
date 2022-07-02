6277a1f92d70e1f995b67c6537b3d4a785564f25
mert <mertcanbenli99@outlook.com>
2022-07-02 18:59:04 +0300

    get image

--- routes/routes.js
+++ routes/routes.js
@@ -1,26 +1,26 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const employeeModel = require("../models/employee");
 const router = express.Router();
 const multer = require("multer");
 const { default: mongoose } = require("mongoose");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './pics')
+    cb(null, "./pics");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, Date.now() + '--' + file.originalname)
-  }
-})
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, Date.now() + "--" + file.originalname);
+  },
+});
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
 router.post("/save-member", upload.single("picture"), uploadFiles);
 
 async function uploadFiles(req, res) {
-  req.body.picture = req.file.filename;
   const employee = new employeeModel(req.body);
+  employee.picture = path.resolve(__dirname + "/../pics/" + req.file.filename);
   try {
     const savedUser = await employee.save();
     console.log(savedUser);
@@ -29,9 +29,21 @@ async function uploadFiles(req, res) {
     res.status(400).json(error);
   }
 }
-// router.get("/get-members", async (req,res => {
-  
-      
-// }))
+router.get("/get-members", async (req, res) => {
+  const users = await employeeModel.find();
+  try {
+    res.send(users);
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get("/get-image/:id", async (req, res) => {
+  const user = await employeeModel.findById(req.params.id);
+  try {
+    res.sendFile(user.picture);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 module.exports = router;
