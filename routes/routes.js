66a806c572e4b718bd90c663fbfa0190caae8c66
atilayeseN <atilayesen@hotmail.com>
2022-07-02 16:07:01 +0300

    add image path

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const path = require('path');
 const employeeModel = require("../models/employee");
 const router = express.Router();
 const multer = require("multer");
 const { default: mongoose } = require("mongoose");
 const upload = multer({ dest: "uploads/" });
 
+
 router.post("/save-member", upload.single("picture"), uploadFiles);
+
+router.get('/someImageUrlOnlyForAuthorizedUsers', function(req,res){
+  res.sendFile(path.resolve(__dirname + '/../uploads/4f7176168fb0e49144443d87ee721796.jpg'));
+
+});
 
 async function uploadFiles(req, res) {
   const employee = new employeeModel(req.body);
@@ -17,9 +24,9 @@ async function uploadFiles(req, res) {
     res.status(400).json(error);
   }
 }
-router.get("/get-members", async (req,res => {
+// router.get("/get-members", async (req,res => {
   
       
-}))
+// }))
 
 module.exports = router;
