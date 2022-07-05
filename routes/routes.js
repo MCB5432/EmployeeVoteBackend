1157122413b859f3aa738be748eff18e5d64bb1f
atilayeseN <atilayesen@hotmail.com>
2022-07-05 20:38:58 +0300

    Revert "add mkdir"
    This reverts commit 483b52f063d2d305540836eaa4c7cd64b2a9d6c7.

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,23 @@ const { default: mongoose } = require("mongoose");
 
 router.post("/save-member",async (req,res) => {
   const employee = new employeeModel(req.body);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./pics");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, Date.now() + "--" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post("/save-member", upload.single("picture"), uploadFiles);
+
+async function uploadFiles(req, res) {
+  const employee = new employeeModel(req.body);
+  employee.picture = path.resolve(__dirname + "/../pics/" + req.file.filename);
   try {
     const savedUser = await employee.save();
     console.log(savedUser);
@@ -14,9 +31,13 @@ router.post("/save-member",async (req,res) => {
   } catch (error) {
     res.status(400).json(error);
   }
+<<<<<<< HEAD
 });
 
 
+=======
+}
+>>>>>>> parent of 483b52f (add mkdir)
 router.get("/get-members", async (req, res) => {
   const users = await employeeModel.find();
   try {
@@ -25,6 +46,18 @@ router.get("/get-members", async (req, res) => {
     console.log(error);
   }
 });
+<<<<<<< HEAD
+=======
+router.get("/get-image/:id", async (req, res) => {
+  const user = await employeeModel.findById(req.params.id);
+
+  try {
+    res.sendFile(user.picture);
+  } catch (error) {
+    res.send(error);
+  }
+});
+>>>>>>> parent of 483b52f (add mkdir)
 
 router.patch("/update-user/:id", async (req, res) => {
   try {
